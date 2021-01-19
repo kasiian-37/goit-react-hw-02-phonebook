3c2b3d5bd639eb2f2588a3e95d7f240dbdf65336
kasiian irina <karamio09@gmail.com>
2021-01-19 18:24:40 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  handleSubmit = contactObj => {
+    if (this.state.contacts.some(({ name }) => name === contactObj.name)) {
+      return alert(`${contactObj.name} already exists in your phonebook`);
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contactObj],
+      };
+    });
+  };
+
+  handleFilterChange = filter => {
+    this.setState({
+      filter,
+    });
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getFilteredContactsList = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContactsList = this.getFilteredContactsList();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmit} />
+
+        <h2>Contacts:</h2>
+        {contacts.length > 1 && (
+          <Filter
+            initialValue={filter}
+            onFilterChange={this.handleFilterChange}
+          />
+        )}
+        {filteredContactsList.length > 0 && (
+          <ContactList
+            contacts={filteredContactsList}
+            onDeleteButtonClick={this.handleDeleteContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
+export default App;
